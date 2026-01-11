10b34ac62bac2fa79f38fcf327c92b661979a452
JoeMJr <josephmiddletonjr@outlook.com>
2026-01-10 22:24:19 -0500

    hasbogoandstalinsort

---
+++ wackysort.py
@@ -0,0 +1,25 @@
+import random
+
+def isSorted(list):
+    return all(list[i] <= list[i+1] for i in range(len(list) - 1)) # Oneliner from stackoverflow
+
+def bogoSort(list):
+    while not isSorted(list):
+        random.shuffle(list)
+    return list
+
+def stalinSort(list):
+    # This is really stupid and I am really stupid
+    count = 0
+    last_num = 0
+    #print((range(len(list)-1)))
+    newlist = []
+    for i in (range(len(list)-1)):
+        if ((last_num > list[count]) & (count > 0)):
+            print("No item added, ", list[count])
+        else:
+            newlist.append(list[count])
+            last_num = list[count]
+        count = count + 1
+    newlist.append(list[count])
+    return newlist
---
+++ wackytest.py
@@ -0,0 +1,9 @@
+import wackysort
+
+test1 = [1, 2, 3, 4, 5]
+test2 = [7, 24, 3, 43, 55]
+test3 = [3, 2, 1, 4, 5]
+test4 = [1, 2, 3, 4, 5]
+test5 = [1, 2, 3, 4, 5]
+
+print(wackysort.stalinSort(test3))
